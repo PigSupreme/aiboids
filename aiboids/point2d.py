737c5ef5c374d73f90a5723b4118cb7439d7b3fe
Michael J. Dancs <MichaelDancs@clayton.edu>
2017-08-01 14:41:24 -0400

    Housekeeping on original Point2d() class: new docs and vdot(a,b).
    This is the original version of Point2d() with better documentation,
    and also added the vdot(a,b) function for vector dot product, with
    the exact same code as __mul__.
    
    This version will still work with the Summer2017 and earlier code,
    but we're going to do some refactoring in the next update cycle that
    may break some of the earlier syntax.

--- aiboids/point2d.py
+++ aiboids/point2d.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
-"""A Two-Dimensional Point/Vector Class.
+"""Two-Dimensional Point/Vector class with common operations.
 
-In addition to the class methods documented below, the following operators
-are available (but not picked up by autodocs):    
-    
-* a + b: Vector Addition
-* -a : Vector Negation
-* a - b: Vector Subtraction
-* a * b: Dot Product (returns a scalar)
-* a / b: Length of orthogonal project of a onto b.
-* a == b: Test for equality
-* a != b: Test for inequality
+Point2d() vectors support standard vector arithmetic: +, - (unary and binary),
+and componentwise test for (in)equality. Multiplication a*b is overloaded with
+the regular dot product; vdot(a,b) is also available. Scalar multiplication is
+done functionally, using `a.scm(k)` for the scalar k and vector a.
+
+Many other operations are available; see below for details.
 """
 
 # for python3 compat
@@ -22,32 +18,42 @@ from __future__ import division
 from math import sqrt, acos, cos, sin
 
 class Point2d(object):
-    """Creates a 2d vector, defaulting to <0,0>.
+    """A two-dimensional vector of floats, defaulting to <0.0, 0.0>.
 
-    Parameters
-    ----------
-    x: float
-        x-coordinate (defaults to 0).
-    y: float
-        y-coordinate (defaults to 0).
+    Args:
+        x (float): x-coordinate (defaults to 0.0).
+        y (float): y-coordinate (defaults to 0.0).
     """
 
-    def __init__(self, x=0, y=0):
+    def __init__(self, x=0.0, y=0.0):
         self.nt = (float(x), float(y))
+
+    def __getitem__(self, index):
+        """Vector components; indexed starting at 0. For internal use only.
+
+        >>> a = Point2d(1,-2)
+        >>> [a[0], a[1]]   # For doctest only; use the syntax below instead.
+        [1.0, -2.0]
+        >>> [a.x, a.y]     # Preferred usage; same result.
+        [1.0, -2.0]
+        >>> a.ntuple       # Use this to get the vector as a python tuple.
+        (1.0, -2.0)
+        """
+        return self.nt[index]
 
     @property
     def x(self):
-        """The first (x) coordinate of this point."""
+        """Get the first (x) coordinate of this point; see example above."""
         return self.nt[0]
-    
+
     @property
     def y(self):
-        """The second (y) coordinate of this point."""
+        """Get the second (y) coordinate of this point; see example above."""
         return self.nt[1]
 
     @property
     def ntuple(self):
-        """This point as a Python tuple."""
+        """Get this point as a Python tuple; see example above."""
         return self.nt
 
     def zero(self):
@@ -60,65 +66,58 @@ class Point2d(object):
         >>> print(a)
         Point2d: <0.000000000, 0.000000000>
         """
-        self.nt = (0,0)
+        self.nt = (0.0, 0.0)
 
     def __len__(self):
+        # For internal use.
         return 2
 
     def __str__(self):
         return "Point2d: <%.9f, %.9f>" % self.nt
 
+    def __eq__(self, other):
+        """Test if two points are equal; overrides the == operator.
+
+        >>> Point2d(1.0,0)==Point2d(1,0)
+        True
+        >>> Point2d(1,0)==Point2d(0,1)
+        False
+        >>> # When we override __eq__, we get inequality for free. Righteous!
+        >>> Point2d(2,3) != Point2d(3,2)
+        True
+        """
+        return self.nt[0] == other.nt[0] and self.nt[1] == other.nt[1]
+
+    def __add__(self, term):
+        """Coordinatewise addition; overrides the + operator.
+
+        >>> a = Point2d(1,-2)
+        >>> b = Point2d(3,5)
+        >>> print(a+b)
+        Point2d: <4.000000000, 3.000000000>
+        >>> a += Point2d(1,1)
+        >>> print(a)
+        Point2d: <2.000000000, -1.000000000>
+        """
+        return Point2d(self.nt[0] + term.nt[0], self.nt[1] + term.nt[1])
+
     def __neg__(self):
         """Negates each entry; overrides unary - operator.
 
-        Example
-        -------
         >>> a = Point2d(1,-2)
         >>> print(-a)
         Point2d: <-1.000000000, 2.000000000>
         """
         return Point2d(-self.nt[0], -self.nt[1])
 
-    def __eq__(self, other):
-        """Test if two points are equal; overrides the == operator.
-
-        Example
-        -------
-        >>> Point2d(1.0,0)==Point2d(1,0)
-        True
-        >>> Point2d(1,0)==Point2d(0,1)
-        False
-        >>> # When we override __eq__, we get inequality for free. Righteous!
-        >>> Point2d(2,3)!=Point2d(3,2)
-        True
-        """
-        return self.nt[0]==other.nt[0] and self.nt[1]==other.nt[1]
-
-    def __add__(self, term):
-        """Coordinatewise addition; overrides the + operator.
-
-        Example
-        -------
-        >>> a = Point2d(1,-2)
-        >>> b = Point2d(3,5)
-        >>> print(a+b)
-        Point2d: <4.000000000, 3.000000000>
-        >>> a+=Point2d(1,1)
-        >>> print(a)
-        Point2d: <2.000000000, -1.000000000>
-        """
-        return Point2d(self.nt[0] + term.nt[0], self.nt[1] + term.nt[1])
-
     def __sub__(self, term):
-        """Coordinatewise subtraction; overrides the - operator.
+        """Coordinatewise subtraction; overrides the binary - operator.
 
-        Example
-        -------
         >>> a = Point2d(1,-2)
         >>> b = Point2d(3,5)
         >>> print(a-b)
         Point2d: <-2.000000000, -7.000000000>
-        >>> a-=Point2d(1,1)
+        >>> a -= Point2d(1,1)
         >>> print(a)
         Point2d: <0.000000000, -3.000000000>
         """
@@ -127,36 +126,16 @@ class Point2d(object):
     def __mul__(self, term):
         """Dot product; overrides the \* operator.
 
-        Example
-        -------
-        >>> Point2d(1,-2)*Point2d(3,5)
+        >>> Point2d(1,-2)*Point2d(3,5)           # As a binary operator
         -7.0
-        >>> # But compound assignment *= should probably be avoided!
-        >>> a = Point2d(1,2)  # This is a vector, for now...
-        >>> a*=a              # ... but the dot product returns a scalar
-        >>> a
-        5.0
+        >>> vdot(Point2d(1,-2), Point2d(3,5))    # As a function of two vectors.
+        -7.0
         """
         return (self.nt[0] * term.nt[0]) + (self.nt[1] * term.nt[1])
 
-    def __getitem__(self, index):
-        """Vector components; indexed starting at 0.
-
-        Example
-        -------
-        >>> a = Point2d(1,-2)
-        >>> a[0]
-        1.0
-        >>> a[1]
-        -2.0
-        """
-        return self.nt[index]
-
     def scm(self, scalar):
-        """Scalar multiplication of this vector.
+        """Scalar multiplication by this vector.
 
-        Example
-        -------
         >>> a = Point2d(1,-2)
         >>> print(a.scm(3.5))
         Point2d: <3.500000000, -7.000000000>
@@ -168,15 +147,10 @@ class Point2d(object):
     def rotated_by(self, angle, use_deg=False):
         """Get this vector rotated anticlockwise.
 
-        Parameters
-        ----------
-        angle: int or float
-            Directed anticlockwise angle to rotate by,
-        degrees: boolean
-            If True, angle is in degrees. Otherwise radians (default)
+        Args:
+            angle (float): Directed anticlockwise angle in radians.
+            use_deg (bool, optional): Set True to use degrees instead of radians.
 
-        Example
-        -------
         >>> from math import pi
         >>> a = Point2d(2,-2)
         >>> print(a.rotated_by(pi))
@@ -196,8 +170,6 @@ class Point2d(object):
     def norm(self):
         """Get the norm (length) of this vector.
 
-        Example
-        -------
         >>> Point2d(1,-2).norm()
         2.23606797749979
         """
@@ -206,8 +178,6 @@ class Point2d(object):
     def sqnorm(self):
         """Get the squared norm (length) of this vector.
 
-        Example
-        -------
         >>> Point2d(1,-2).sqnorm()
         5.0
         """
@@ -216,12 +186,12 @@ class Point2d(object):
     def unit(self):
         """Get a unit vector in the same direction as this one.
 
-        Note
-        ----
-        Be aware of round-off errors; see the example below.
+        Raises:
+            ZeroDivisionError: If called on a zero vector.
 
-        Example
-        -------
+        Note:
+            Be aware of round-off errors; see the example below.
+
         >>> a = Point2d(1,-2)
         >>> print(a.unit())
         Point2d: <0.447213595, -0.894427191>
@@ -233,12 +203,12 @@ class Point2d(object):
     def normalize(self):
         """Rescale this vector to have length 1.
 
-        Note
-        ----
-        Be aware of round-off errors; see the example below.
+        Raises:
+            ZeroDivisionError: If called on a zero vector.
 
-        Example
-        -------
+        Note:
+            Be aware of round-off errors; see the example below.
+
         >>> a = Point2d(1,-2)
         >>> a.normalize()
         >>> print(a)
@@ -250,21 +220,15 @@ class Point2d(object):
         self.nt = (float(self.nt[0]/r), float(self.nt[1]/r))
 
     def truncate(self, maxlength):
-        """Rescale this vector if needed so its length is not too large.
+        """Rescale this vector in-place to a maximum length.
 
-        Parameters
-        ----------
-        maxlength: float
-            Upper limit on the length. If the current length exceeds this,
-            the vector will be rescaled.
+        Args:
+            maxlength (float): Upper limit on the length. If the current length
+            exceeds this, the vector is rescaled in-place.
 
-        Returns
-        -------
-        bool:
-            True if rescaling was done, False otherwise.
+        Returns:
+            (bool): True if rescaling was done, False otherwise.
 
-        Examples
-        --------
         >>> a = Point2d(1,-2)
         >>> a.truncate(1.0)
         True
@@ -284,34 +248,28 @@ class Point2d(object):
             return False
 
     def scale_to(self, mag):
-        """Change this vector's scale to the given magnitude.
+        """Change this vector in-place to have the given magnitude.
 
-        Parameters
-        ----------
-        mag: float
-            New magnitude for this vector; negative will reverse direction.
+        Args:
+            mag (float): New magnitude; negative value also reverses direction.
 
-        Example
-        -------
-        >>> a = Point2d(2,3)
-        >>> a.scale_to(-4.2)
-        >>> print(a)
-        Point2d: <-2.329740824, -3.494611236>
-        >>> a.norm()
-        4.2
+        >>> a = Point2d(3,4)
+        >>> a.scale_to(10)
+        >>> print(a, a.norm())
+        Point2d: <6.000000000, 8.000000000> 10.0
+        >>> a.scale_to(-0.5)
+        >>> print(a, a.norm())
+        Point2d: <-0.300000000, -0.400000000> 0.5
         """
-        self.normalize()
+        mag = mag/self.norm()
         self.nt = (mag*self.nt[0], mag*self.nt[1])
 
     def angle(self):
         """Get the polar angle of this vector in radians; range (-pi,pi]
 
-        Raises
-        ------
-        ZeroDivisionError: If called on a zero vector.
+        Raises:
+            ZeroDivisionError: If called on a zero vector.
 
-        Examples
-        --------
         >>> Point2d(1,-2).angle()
         -1.1071487177940904
         >>> Point2d(1,0).angle()
@@ -335,26 +293,18 @@ class Point2d(object):
     def __truediv__(self, direction):
         """Length of an orthogonal projection; overrides the / operator.
 
-        Parameters
-        ----------
-        direction: Point2d
-            The vector we project onto; not required to be a unit vector.
+        Args:
+            direction (Point2d): The vector we project onto.
 
-        Returns
-        -------
-        float:
-            The length of the projection vector.
+        Returns:
+            float: The length of the orthogonal projection.
 
-        Notes
-        -----
-        Returns the scalar q such that self = q*v2 + v3, where v2 is in the
-        span of direction and v2 and v3 are orthogonal. This is algebraically
-        identical to exact division (/).
+        Note:
+            Returns the scalar q such that self = q*v2 + v3, where v2 is in the
+            span of direction  and v2 is orthogonal to v3. This is algebraically
+            identical to exact division (/). If you want the result as a vector,
+            use Point2d.proj(direction) instead.
 
-        If you want the result as a vector, use Point2d.proj(direction) instead.
-
-        Examples
-        --------
         >>> a = Point2d(2,2)
         >>> b = Point2d(3,0)
         >>> a/b
@@ -374,29 +324,22 @@ class Point2d(object):
     def proj(self, direction):
         """Get the orthogonal projection of this vector onto another.
 
-        Parameters
-        ----------
-        direction: Point2d
-            The vector we project onto; not required to be a unit vector.
+        Args:
+            direction (Point2d): The vector we project onto.
 
-        Returns
-        -------
-        Point2d
-            The unique vector v2 such that self = q*v2 + v3, where v2 is in the
-            span of direction and v2 and v3 are orthogonal.
+        Returns:
+            Point2d: The unique vector v2 such that self = q*v2 + v3, where v2
+            is a multiple of direction  and v2 is orthogonal to v3.
 
-        Example
-        -------
+        Note:
+            If you want both v2 and v3, use Point2d.resolve(direction) instead.
+
         >>> a = Point2d(2,4)
         >>> b = Point2d(3,-2)
         >>> print(a.proj(b))
         Point2d: <-0.461538462, 0.307692308>
         >>> print(b.proj(a))
         Point2d: <-0.200000000, -0.400000000>
-
-        Notes
-        -----
-        If you want both v2 and v3, use Point2d.resolve(direction) instead.
         """
         # Note: * is the dot product, using __mul__ to override above.
         r = (self*direction)/direction.sqnorm()
@@ -406,19 +349,13 @@ class Point2d(object):
     def resolve(self, direction):
         """Orthogonal decomposition of this vector in a given direction.
 
-        Parameters
-        ----------
-        direction: Point2d
-            The vector we project onto; not required to be a unit vector.
+        Args:
+            direction (Point2d): The vector we project onto.
 
-        Returns
-        -------
-        Point2d, Point2d:
-            v2,v3 such that self = q*v2 + v3, where v2 is in the
-            span of direction and v2 and v3 are orthogonal.
+        Returns:
+            Point2d, Point2d: v2, v3 such that self = q*v2 + v3, where v2 is a
+            multiple of direction and v2 is orthogonal to v3.
 
-        Example
-        -------
         >>> a = Point2d(2,-3)
         >>> b = Point2d(1,4)
         >>> print(a.resolve(b)[0])
@@ -435,8 +372,6 @@ class Point2d(object):
     def left_normal(self):
         """Returns the left-facing normal of this vector.
 
-        Example
-        -------
         >>> print(Point2d(1,-2).left_normal())
         Point2d: <2.000000000, 1.000000000>
         """
@@ -446,8 +381,9 @@ class Point2d(object):
         """Allows a value to be assigned to each vector components;
         indexed starting at 0.
 
-        Example
-        -------
+        Note:
+            Internal use only.
+
         >>> a = Point2d(1, -2)
         >>> print(a)
         Point2d: <1.000000000, -2.000000000>
@@ -464,6 +400,16 @@ class Point2d(object):
             self.nt = (self.nt[0], value)
         else:
             raise KeyError("Point2d %s has no component %s" % (self, str(index)))
+
+def vdot(v1, v2):
+    """Dot product of two vectors; functional form.
+
+    >>> Point2d(1,-2)*Point2d(3,5)           # As a binary operator
+    -7.0
+    >>> vdot(Point2d(1,-2), Point2d(3,5))    # As a function of two vectors.
+    -7.0
+    """
+    return (v1[0] * v2[0]) + (v1[1] * v2[1])
 
 if __name__ == "__main__":
     print("Point2d functions. Import this module elsewhere.")
---
+++ docs/vpoints.rst
@@ -0,0 +1,9 @@
+Code for points/vectors
+=======================
+
+Two-Dimensional Vectors (point2d.py)
+------------------------------------
+
+.. automodule:: point2d
+   :special-members:
+
