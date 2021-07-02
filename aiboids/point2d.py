b9cfc377a3962a36dbe3ce4c91946a4ee1f6af8f
Pig Supreme <PigSupreme2017@gmail.com>
2021-07-02 14:01:30 -0400

    Checking if future imports is the culprit.

--- aiboids/point2d.py
+++ aiboids/point2d.py
@@ -13,12 +13,6 @@ Todo:
     code for __mul__ (dot product) for an example of what we should be doing.
 """
 
-# for python3 compat
-from __future__ import unicode_literals
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import division
-
 from math import sqrt, acos, cos, sin, isnan
 
 class Point2d(object):
